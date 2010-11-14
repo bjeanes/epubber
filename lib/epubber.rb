4b6d076a2d0e047b71bbef9b21b3d2de19edaafe
Bodaniel Jeanes <me@bjeanes.com>
2010-11-14 19:52:22 +0800

    Break out executable code into own binary

--- lib/epubber.rb
+++ lib/epubber.rb
@@ -10,13 +10,3 @@ require "book"
 ROOT_PATH   = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 BUILD_PATH  = File.join(ROOT_PATH, "tmp", "build")
 OUTPUT_PATH = File.join(ROOT_PATH, "tmp", "output")
-
-if __FILE__ == $0
-  abort "Please provide path to PeepCode book as first argument" unless ARGV.size > 0
-
-  book_path = ARGV.shift
-  book = Epubber::Book.new(book_path, BUILD_PATH, OUTPUT_PATH)
-  location = book.save
-
-  puts "#{File.expand_path(book_path)} saved to #{File.expand_path(location)}"
-end
